25529dcd1702988f1fb1b38bdc388e07042d000f
keigodasu <keigodasu0524@yahoo.co.jp>
2021-01-09 19:23:40 +0900

    complete

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
 	"time"
 )
 
@@ -71,4 +72,23 @@ func (s *SpySleeper) Sleep() {
 
 func main() {
 	Countdown(os.Stdout, &ConfigurableSleeper{1 * time.Second, time.Sleep})
+}
+
+func fizzBuzz(n int) []string {
+	list := []string{}
+
+	for i := 0; i < n; i++ {
+		switch {
+		case i%3 == 0 && i%5 != 0:
+			list = append(list, "Fizz")
+		case i%3 != 0 && i%5 == 0:
+			list = append(list, "Buzz")
+		case i%3 == 0 && i%5 == 0:
+			list = append(list, "FizzBuzz")
+		default:
+			list = append(list, strconv.Itoa(i))
+		}
+	}
+
+	return list
 }
