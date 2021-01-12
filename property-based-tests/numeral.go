79dd8bcf70d6d8c2e732cf5c23ecaf0793c63dbb
keigodasu <keigodasu0524@yahoo.co.jp>
2021-01-12 21:05:20 +0900

    complete part of property based tests

---
+++ property-based-tests/numeral.go
@@ -0,0 +1,78 @@
+package property_based_tests
+
+import "strings"
+
+type RomanNumeral struct {
+	Value  uint16
+	Symbol string
+}
+
+type RomanNumerals []RomanNumeral
+
+var allRomanNumerals = RomanNumerals{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
+func (r RomanNumerals) ValueOf(symbols ...byte) uint16 {
+	symbol := string(symbols)
+	for _, s := range r {
+		if s.Symbol == symbol {
+			return s.Value
+		}
+	}
+
+	return 0
+}
+
+func ConvertToArabic(roman string) uint16 {
+	var total uint16
+	total = 0
+
+	for i := 0; i < len(roman); i++ {
+		symbol := roman[i]
+
+		if couldBeSubtractive(i, symbol, roman) {
+			if value := allRomanNumerals.ValueOf(symbol, roman[i+1]); value != 0 {
+				total += value
+				i++
+			} else {
+				total++
+			}
+		} else {
+			total += allRomanNumerals.ValueOf(symbol)
+		}
+
+	}
+
+	return total
+}
+
+func couldBeSubtractive(index int, currentSymbol uint8, roman string) bool {
+	isSubtractiveSymbol := currentSymbol == 'I' || currentSymbol == 'X' || currentSymbol == 'C'
+	return index+1 < len(roman) && isSubtractiveSymbol
+}
+
+func ConvertToRoman(arabic uint16) string {
+	var result strings.Builder
+
+	for _, numeral := range allRomanNumerals {
+		for arabic >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			arabic -= numeral.Value
+		}
+	}
+
+	return result.String()
+}
